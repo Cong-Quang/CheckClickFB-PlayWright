438af3029bce8ba2bfccedcfd4470b83f11de10b
Cong-Quang <nguyencongquang02082003@gmail.com>
2022-02-13 10:41:32 +0700

    Add project files.

---
+++ ToolsText/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Playwright;
+
+using var playwright = await Playwright.CreateAsync();
+var chromium = playwright.Chromium;
+var browser = await chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false});
+var context = await browser.NewContextAsync();
+await context.RouteAsync("**/*", route => route.ContinueAsync());
+var page = await context.NewPageAsync();
+await page.PauseAsync();
